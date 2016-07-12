44112aa0aa924f3962092eac11bb543b0fc7fee1
liangbing <909901234@qq.com>
2016-07-12 15:10:36 +0800

    Initial Commit

---
+++ UserCollectionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UserCollectionDemo
+//
+//  Created by liangbing on 16/7/12.
+//  Copyright © 2016年 liangxianhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
